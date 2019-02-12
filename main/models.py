0a6dfe55c5b11556c1803ee201ba20cf51640b98
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2019-02-12 11:58:17 +0100

    #23 La til modeller for database
    User, Event og Attendee

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_level = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=30)
+    accept_notifications = models.BooleanField()
+    accept_newsletter = models.BooleanField()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    organizer = models.ForeignKey(User, models.SET_NULL)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=30)
+    price = models.DecimalField()
+    
+
+class Attendee(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    event = models.ForeignKey(Event, models.CASCADE)
+    has_paid = models.BooleanField()
+
