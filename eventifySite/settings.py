94b3332526e81d45ce28488ffe8bc6b724f6f9fa
Kasper <kasper@dhcp-10-22-13-241.wlan.ntnu.no>
2019-02-12 13:02:19 +0100

    Endret databasenavnet

--- eventifySite/settings.py
+++ eventifySite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'eventifySite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'eventifydatabase',
+        'NAME': 'eventify-db',
         'USER': 'myDatabaseUser',
         'PASSWORD': 'programvareutvikling32',
         'HOST': 'localhost',
