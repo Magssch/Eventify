1063b866ba2a851f4ce16be58dbe4779c577ff2d
Petter <petter@Petters-MacBook-Pro.local>
2019-02-19 22:33:25 +0100

    Updating templates dir

--- eventifySite/settings.py
+++ eventifySite/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'eventifySite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR), 'main/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
