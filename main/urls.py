7a73cff62b8a06a6629a75291ac1cffac66fb38f
aadnekar <aadne.karstad@gmail.com>
2019-02-19 09:37:38 +0100

    #22 - La til app_name = 'main'

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# To reference the app, e.g. in django.shortcuts.redirect('main:homepage')
+app_name = 'main'
+
 urlpatterns = [
     path('', views.homepage, name="homepage")
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ from .models import Event
 # Create your views here.
 
 def homepage(request):
+
+
+	
 	return render(request = request,
 				  template_name = "main/homepage.html",
 				  context = {"events":Event.objects.all})
