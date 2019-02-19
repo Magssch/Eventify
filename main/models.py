8ee3981958b0f6832554c3e84584cdba56d3c778
Petter <petter@dhcp-10-220-217-190.wlan.ntnu.no>
2019-02-19 19:38:00 +0100

    Updating database settings again..

--- main/models.py
+++ main/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 
-#class User(models.Model):
-#    first_name = models.CharField(max_length=30)
-#    last_name = models.CharField(max_length=30)
-#    user_level = models.IntegerField()
-#    email = models.EmailField(max_length=50)
-#    phone = models.IntegerField()
-#    address = models.CharField(max_length=30)
-#    accept_notifications = models.BooleanField()
-#    accept_newsletter = models.BooleanField()
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_level = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    phone = models.IntegerField()
+    address = models.CharField(max_length=30)
+    accept_notifications = models.BooleanField()
+    accept_newsletter = models.BooleanField()
 
-#    def __str__(self):
-#        return self.first_name + " " + self.last_name
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 
-#class Event(models.Model):
-#    name = models.CharField(max_length=30)
-#    organizer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-#    date = models.DateTimeField()
-#    location = models.CharField(max_length=30)
-#    price = models.IntegerField()
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    organizer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=30)
+    price = models.IntegerField()
 
-#    def __str__(self):
-#        return self.name
+    def __str__(self):
+        return self.name
 
     
 
-#class Attendee(models.Model):
-#    user = models.ForeignKey(User, models.CASCADE)
-#    event = models.ForeignKey(Event, models.CASCADE)
-#    has_paid = models.BooleanField()
+class Attendee(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    event = models.ForeignKey(Event, models.CASCADE)
+    has_paid = models.BooleanField()
 
