8b6e8204478d2a8fc1e918f963efca43e66699e2
Petter <petter@dhcp-10-220-217-190.wlan.ntnu.no>
2019-02-19 17:11:57 +0100

    Updating database in settings.py

--- eventifySite/settings.py
+++ eventifySite/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'eventifySite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'eventify-db',
         'USER': 'myDatabaseUser',
         'PASSWORD': 'programvareutvikling32',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
