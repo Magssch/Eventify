c16965a15197e010c6d19cd7b29b5e9df2fd65c0
Petter <petter@Petters-MacBook-Pro.local>
2019-02-28 23:02:46 +0100

    Tests for RegistrationForm

---
+++ main/tests/test_forms.py
@@ -0,0 +1,33 @@
+from django.test import TestCase, RequestFactory
+from django.test import Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import HttpRequest
+from main.forms import EditProfileForm
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.contrib.messages.middleware import MessageMiddleware
+
+from main.forms import * 
+
+class FormTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.username = 'myuser'
+        self.password = 'valid_password1'
+        self.user = User.objects.create_user(self.username, 'email@test.com', self.password, is_staff=False)
+        self.client = Client()
+
+    def test_registration_form_saves_data_to_instance_user_on_save(self):
+        """ Tests that the form saves name, email to user object when committing form"""
+        form = RegistrationForm(data={'check': True, 'username':'user2',
+        'first_name':'user',
+        'last_name':'userlast',
+        'email':'email@test.com',
+        'password1':self.password,
+        'password2':self.password})
+        self.assertTrue(form.is_valid())
+        person = form.save()
+        self.assertEqual(person.first_name, 'user')
+        
+
