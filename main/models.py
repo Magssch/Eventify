08893fc41bd1e7df8c939afdb32fee9f8452cd93
Lars-Magnus Underhaug <larsmagu@gmail.com>
2019-03-12 11:53:08 +0100

    id9_paamelding
    Laget model for påmelding av brukere til arrangement

--- main/models.py
+++ main/models.py
@@ -18,10 +18,26 @@ class Event(models.Model):
         return reverse("event_info", kwargs={"my_id": self.id})
     
 
+"""
+Model that holds list of all attendees for events. Uses foreign key from user and Event 
+to connect the tables. verbose_name adds label to database field making it more human relatable
+while models.CASCADE ensure that on_delete both the user and event connection is deleted.
+"""
 class Attendee(models.Model):
-    user = models.ForeignKey(User, models.CASCADE)
-    event = models.ForeignKey(Event, models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Event', models.CASCADE)
+    event = models.ForeignKey(Event, verbose_name='Attendee' models.CASCADE)
     has_paid = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Attendee for Event'
+        verbose_name_plural = 'Attendees for Events'
+        unique_together = ('event', 'user')
+
+    def save(self, *args, **kwargs):
+        super(Attendee, self).save(*args, **kwargs)
 
 
 
